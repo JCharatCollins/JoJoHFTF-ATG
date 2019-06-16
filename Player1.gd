f84658a84f88a9a899959abff95f1ab24fb4a6f9
JaceBeleren101 <41838497+JaceBeleren101@users.noreply.github.com>
2019-06-16 16:00:27 -0700

    Add files via upload

--- Player1.gd
+++ Player1.gd
@@ -1,5 +1,6 @@
 extends KinematicBody
 
+const Speed = 0.5
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,31 +11,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	var velocity = Vector3()
+	var x_movement : float = Input.get_action_strength("player1_right") - Input.get_action_strength("player1_left")
+	velocity.x = x_movement * Speed
+	move_and_collide(velocity)
+	
 	print(translate_inputs(Input.get_action_strength("player1_up"), Input.get_action_strength("player1_down"), Input.get_action_strength("player1_left"), Input.get_action_strength("player1_right")))
 
 func translate_inputs(up , down, left, right):
-	var vertical_dir = up-down
-	var horizontal_dir = right-left
-	if (vertical_dir == 1):
-		if (horizontal_dir == 1):
-			return 9
-		elif (horizontal_dir == -1):
-			return 7
-		elif (horizontal_dir == 0):
-			return 8
-	elif (vertical_dir == -1):
-		if (horizontal_dir == 1):
-			return 3
-		elif (horizontal_dir == -1):
-			return 1
-		elif (horizontal_dir == 0):
-			return 2
-	elif (vertical_dir == 0):
-		if (horizontal_dir == 1):
-			return 6
-		elif (horizontal_dir == -1):
-			return 4
-		elif (horizontal_dir == 0):
-			return 5
-	
-
+	var inputs = [up-down, right-left]
+	var inputstonotation = {[1.0, 1.0] : 9, [1.0, 0.0] : 8, [1.0, -1] : 7, [0.0, 1.0] : 6, [0.0, 0.0] : 5, [0.0, -1] : 4, [-1.0, 1.0] : 3, [-1.0, 0.0] : 2, [-1.0, -1.0] : 1}
+	return inputstonotation[inputs]
